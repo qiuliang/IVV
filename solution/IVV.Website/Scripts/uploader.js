54fb7905f0e265fc8956aff6f0728f3cc0cc2551
QiuLiang <star.qiul@gmail.com>
2013-06-09 21:56:20 +0800

    uploadify style update

--- solution/IVV.Website/Scripts/uploader.js
+++ solution/IVV.Website/Scripts/uploader.js
@@ -7,14 +7,14 @@ var uploader = function (obj) {
 
 uploader.prototype.init = function () {
     var _self = this;
-    $("#" + _self._param.dialogId).append('<iframe id="uploadFrame" src="/admin/upload.aspx?t=' + Math.random() + '" style="width:100%;height:380px;"></iframe>');
+    $("#" + _self._param.dialogId).append('<iframe id="uploadFrame" src="/admin/upload.aspx?t=' + Math.random() + '" style="width:100%;height:380px;border:0px;"></iframe>');
     $("#" + _self._param.triggerId).click(function () {
         $("#" + _self._param.dialogId).dialog({
             width : 650,
             autoOpen : true,
             buttons: [
                 {
-                    text: "Ok",
+                    text: "确定",
                     click: function () {
                         var urls = window.frames["uploadFrame"].document.__getUploadFileUrls();
                         for (var i = 0; i < urls.length; i++) {
